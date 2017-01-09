00ffc86575e9a57c5b62123ecdce0166c395fc90
MAI ICHINOKI <maimode080653@gmail.com>
2017-01-09 16:45:16 +0800

    seed_sns

--- design/dbconnect.php
+++ design/dbconnect.php
@@ -1,7 +1,7 @@
   <?php
-  $db=mysqli_connect('localhost','root','mysql','seed_sns') or die (mysqli_connect_error());
+  $db = mysqli_connect('localhost','root','mysql','seed_sns') or die (mysqli_connect_error());
   mysqli_set_charset($db,'utf8');
   
-  
+  date_default_timezone_set('Asia/Manila');
 
   ?>
